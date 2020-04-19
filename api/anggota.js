f15e3f8e297245af299a4e36e9b6b256e7385dea
naraditanabila <42129351+naraditanabila@users.noreply.github.com>
2020-04-19 10:31:21 +0700

    update folder api

--- api/anggota.js
+++ api/anggota.js
@@ -13,6 +13,63 @@ const dbPromise = new Pool({
     ssl: false
 })
 
-//add anggota baru
-//cek validasi anggota berdasarkan id anggota
-//menampilkan data anggota
+//add anggota baru (POST)
+app.post('/register',async(req,res) => {
+    try {
+        const id_umum = req.params.id_umum
+        const {nama, alamat, no_hp, email, pekerjaan} = req.body
+        await dbPromise.query(`insert into umum(id_umum, nama, alamat, no_hp, email, pekerjaan)
+            values('${id_umum}','${nama}','${alamat}','${no_hp}','${email}','${pekerjaan}')`)
+        console.log(req.body)
+        res.json('Data anggota baru berhasil ditambahkan')
+    } catch (error) {
+        console.log(error)
+        res.json('error')
+    }
+})
+
+//cek validasi anggota berdasarkan id umum (GET)
+app.get('/umum/:id_umum', async(req,res) => {
+    let ret;
+    const id_umum = req.params.id_umum
+    dbPromise.query('SELECT anggota.id_anggota, umum.id_umum, umum.nama, umum.alamat, umum.no_hp, umum.email, umum.pekerjaan FROM umum, anggota WHERE id_umum=$1 and umum.id_umum=anggota.id_umum',[id_umum], (err,result) => {
+        if (!err){
+            ret={
+                status:200,
+                result: result.rows
+            };
+            res.status(200).json(ret)
+        }
+        else {
+            ret={
+                status:err.code,
+                result: 'nomor identitas tidak terdaftar'
+            };
+            res.json(ret)
+        }
+    })
+})
+
+//cek validasi anggota berdasarkan nim (GET)
+app.get('/mahasiswa/:nim', async(req,res) => {
+    let ret;
+    const nim = req.params.nim
+    dbPromise.query('SELECT  anggota.id_anggota, mahasiswa.nim, mahasiswa.nama, mahasiswa.fakultas, mahasiswa.prodi, mahasiswa.angkatan FROM anggota,mahasiswa where nim=$1 and mahasiswa.nim=anggota.nim',[nim], (err,result) => {
+        if (!err){
+            ret={
+                status:200,
+                result: result.rows
+            };
+            res.status(200).json(ret)
+        }
+        else {
+            ret={
+                status:err.code,
+                result: 'nim tidak valid'
+            };
+            res.json(ret)
+        }
+    })
+})
+
+module.exports = app;
--- index.js
+++ index.js
@@ -1,27 +1,30 @@
 const Express = require('express')
 const bodyParser = require('body-parser')
+const cors = require('cors')
+const port = 3001
+const app = Express()
 const Pool = require('pg').Pool
 require('dotenv').config()
 
-const app = Express()
-const port = 3000
+/*const db = new Pool({
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  port: process.env.DB_PORT,
+  ssl: false
+})*/
 
-const db = new Pool({
-    host: process.env.DB_HOST,
-    database: process.env.DB_NAME,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    port: process.env.DB_PORT,
-    ssl: false
-})
+const anggotaRouter = require('./api/anggota')
+const bukuRouter = require('./api/buku')
+const loginRouter = require('./api/login')
+const peminjamanRouter = require('./api/peminjaman')
 
-var anggotaRouter = require('./api/anggota')
-var bukuRouter = require('./api/buku')
-var loginRouter = require('./api/login')
-var peminjamanRouter = require('./api/peminjaman')
-
-db.connect()
+//db.connect()
 app.use(bodyParser())
+app.use(cors())
+app.use(bodyParser())
+app.use(cors())
 
 app.use(anggotaRouter)
 app.use(bukuRouter)

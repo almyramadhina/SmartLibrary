72085d4acc794481d40dbfe3c1fe511afe3f6a30
dwidnw <55821468+dwidnw@users.noreply.github.com>
2020-04-22 20:01:49 +0700

    Update anggota.js

--- frontend/js/anggota.js
+++ frontend/js/anggota.js
@@ -17,3 +17,57 @@ const addUmum = async() => {
 	const json = await response.json();
 	console.log(json);
 }
+
+function showDataSearch() {
+		if ($("#smahasiswa").is(":checked")){
+            document.getElementById('tabelUmum').style.display='none';
+			$(document).ready(function(){
+				$.ajax({
+                    url: 'https://itbsmartlibrary.herokuapp.com/anggota/mahasiswa/'+encodeURIComponent(document.getElementById('searchData').value),
+                    type:'GET',
+                    dataType: 'json',
+                    success: function(response) {
+						console.log(response);
+							content = '';
+							var isitab = document.getElementById('tabbodyMahasiswa')
+							content+=
+							    "<tr>" +
+								    "<td>"+response.result[0].nim+"\n"+
+									"<td>"+response.result[0].nama+"\n"+
+                                    "<td>"+response.result[0].fakultas+"\n"+
+                                    "<td>"+response.result[0].prodi+"\n"+
+                                    "<td>"+response.result[0].angkatan+"\n"+
+                                    "<td>"+response.result[0].status_aktif+"\n"+
+								"</td><tr/>";
+								isitab.innerHTML = content;
+				
+				      }
+				})
+			 });
+		}
+        else {
+            document.getElementById('tabelMahasiswa').style.display='none';
+			$(document).ready(function(){
+				$.ajax({
+                    url: 'https://itbsmartlibrary.herokuapp.com/anggota/umum/'+encodeURIComponent(document.getElementById('searchData').value),
+                    type:'GET',
+                    dataType: 'json',
+                    success: function(response) {
+				console.log(response);
+				content = '';
+				var isitab = document.getElementById('tabbodyUmum')
+					content+=
+						"<tr>" +
+						"<td>"+response.result[0].id_umum+"\n"+
+						"<td>"+response.result[0].nama+"\n"+
+                                   		"<td>"+response.result[0].alamat+"\n"+
+                                    		"<td>"+response.result[0].no_hp+"\n"+
+                                    		"<td>"+response.result[0].email+"\n"+
+                                    		"<td>"+response.result[0].pekerjaan+"\n"+
+						"</td><tr/>";
+					isitab.innerHTML = content;
+				}
+			});
+		});
+	 };
+	}

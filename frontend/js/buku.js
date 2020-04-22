3a7b83429d66ecd0c67f99b5cab5f0e0ef1742fb
dwidnw <55821468+dwidnw@users.noreply.github.com>
2020-04-22 19:56:30 +0700

    Update buku.js

--- frontend/js/buku.js
+++ frontend/js/buku.js
@@ -17,3 +17,57 @@ const addBuku = async() => {
 	const json = await response.json();
 	console.log(json);
 }
+
+function showTabelSearch() {
+    	var x = document.getElementById("tabelBuku");
+		if ($("#sjudul").is(":checked")){
+			$(document).ready(function(){
+				$.ajax({
+                    url: 'https://itbsmartlibrary.herokuapp.com/buku/search/title/'+encodeURIComponent(document.getElementById('searchBuku').value),
+                    type:'GET',
+                    dataType: 'json',
+                    success: function(response) {
+						console.log(response);
+							content = '';
+							var isitab = document.getElementById('tabbodyBuku')
+							content+=
+							    "<tr>" +
+								    "<td>"+response.result[0].id_buku+"\n"+
+									"<td>"+response.result[0].judul+"\n"+
+                                    "<td>"+response.result[0].author+"\n"+
+                                    "<td>"+response.result[0].lokasi+"\n"+
+                                    "<td>"+response.result[0].jml_total+"\n"+
+                                    "<td>"+response.result[0].jml_pinjam+"\n"+
+                                    "<td>"+response.result[0].jml_avail+"\n"+
+								"</td><tr/>";
+								isitab.innerHTML = content;
+				    }
+				})
+			});
+		}
+        else {
+			$(document).ready(function(){
+				$.ajax({
+                    url: 'https://itbsmartlibrary.herokuapp.com/buku/search/author/'+encodeURIComponent(document.getElementById('searchBuku').value),
+                    type:'GET',
+                    dataType: 'json',
+                    success: function(response) {
+						console.log(response);
+							content = '';
+							var isitab = document.getElementById('tabbodyBuku')
+							content+=
+								"<tr>" +
+								 "<td>"+response.result[0].id_buku+"\n"+
+									"<td>"+response.result[0].judul+"\n"+
+                                    "<td>"+response.result[0].author+"\n"+
+                                    "<td>"+response.result[0].lokasi+"\n"+
+                                    "<td>"+response.result[0].jml_total+"\n"+
+                                    "<td>"+response.result[0].jml_pinjam+"\n"+
+                                    "<td>"+response.result[0].jml_avail+"\n"+
+								"</td><tr/>";
+							isitab.innerHTML = content;
+							}
+				});
+				});
+		   };
+	}
